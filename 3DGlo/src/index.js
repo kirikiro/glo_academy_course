e58721cbfdd021c7806ef02986bec2c98f1bd4ff
kirikiro <flameeugenesharki@gmail.com>
2026-05-16 19:25:22 +0300

    silder.js

--- 3DGlo/src/index.js
+++ 3DGlo/src/index.js
@@ -3,6 +3,7 @@ import './modules/menu.js';
 import './modules/modal.js';
 import './modules/reg.js'
 import './modules/tabs.js'
+import './modules/slider.js'
 
 // one("10 may 2026");
 console.log('index.js запущен');
